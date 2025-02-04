f744b1456ab838d0346fcf68eb9688d4ff3c3449
SyedDanyal190 <dk179994@gmail.com>
2025-02-04 16:51:55 +0500

    change docker  file copy from docker  machine  ,data  inside the   all parking files

--- backend/Server.js
+++ backend/Server.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 4002;
+const port = process.env.PORT || 4001;
+
 
 // Middleware to parse JSON requests
 app.use(express.json());
